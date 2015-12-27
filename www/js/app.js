ef6c0d6c172dc5be434758ec2e67497ebfed1593
Hassan Ali <helfi92@gmail.com>
2015-12-27 18:23:25 -0500

    todo app

---
+++ www/js/app.js
@@ -0,0 +1,54 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('starter', ['ionic','firebase','ngCordova'])
+.factory('Items', ['$firebaseArray', function($firebaseArray){
+  var itemRef = new Firebase('https://sizzling-heat-6865.firebaseio.com/items');
+  return $firebaseArray(itemRef);
+}])
+
+
+
+.controller('ListCtrl', function($scope,$ionicListDelegate,$cordovaDialogs,Items){
+  $scope.items = Items;
+
+  $scope.addItem = function(){
+    $cordovaDialogs.prompt('What do you need to buy?', 'Grocery Keeper',['Cancel', 'Add'], '')
+    .then(function(result){
+      if(result.buttonIndex == 2){
+        $scope.items.$add({
+          'name' : result.input1
+        });
+      } 
+    })
+   
+  };
+  $scope.purchaseItem = function(item){
+    var itemRef = new Firebase('https://sizzling-heat-6865.firebaseio.com/items/' + item.$id);
+    itemRef.child('status').set('purchased');
+    $ionicListDelegate.closeOptionButtons();
+  }
+})
+
+
+
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+
+      // Don't remove this line unless you know what you are doing. It stops the viewport
+      // from snapping when text inputs are focused. Ionic handles this internally for
+      // a much nicer keyboard experience.
+      cordova.plugins.Keyboard.disableScroll(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
